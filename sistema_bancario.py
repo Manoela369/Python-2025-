e4ea09c95017f118b4a8bc49c968226379c5e2bc
Manoela369 <manoela.geres@escola.pr.gov.br>
2025-03-28 11:15:43 -0300

    Criação do menu

---
+++ sistema_bancario.py
@@ -0,0 +1,15 @@
+#print("****************Menu****************")
+#print("Criar usuário")
+#print("Depositar")
+#print("Sacar")
+def menu():
+    print('''
+    ****************Menu****************
+    (1) Criar Usuário
+    (2)Criar Conta
+    (3)Listar Contas
+    (d)Depositar 
+    (s)Sacar
+    (q)
+    ''')
+    return input('Qual opção deseja?')
